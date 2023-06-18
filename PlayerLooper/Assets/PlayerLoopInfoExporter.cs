f31aba8cc35f8351bbb5b4d83fe1d25a9622e2d3
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-06-18 23:19:50 +0900

    Add player loop info

---
+++ PlayerLooper/Assets/PlayerLoopInfoExporter.cs
@@ -0,0 +1,38 @@
+using System.IO;
+using System.Text;
+using UnityEngine;
+using UnityEngine.LowLevel;
+
+namespace PlayerLooper.Utility
+{
+    public class PlayerLoopInfoExporter
+    {
+        public static void Export()
+        {
+            var sb = new StringBuilder();
+
+            sb.AppendLine($"Unity {Application.unityVersion}");
+            sb.AppendLine();
+
+            var playerLoop = PlayerLoop.GetCurrentPlayerLoop();
+            
+            foreach (var header in playerLoop.subSystemList)
+            {
+                sb.AppendLine($"---------- {header.type.Name} ----------");
+                
+                foreach (var subSystem in header.subSystemList)
+                {
+                    sb.AppendLine($"{header.type.Name}.{subSystem.type.Name}");
+                    
+                    if (subSystem.subSystemList != null)
+                    {
+                        Debug.LogError($"{header.type.Name}.{subSystem.type.Name} has more subsystems: {subSystem.subSystemList.Length}");
+                    }
+                }
+            }
+            
+            var filePath = Path.Combine(Application.dataPath, $"PlayerLoopInfo_{Application.unityVersion}.txt");
+            File.WriteAllText(filePath, sb.ToString());
+        }
+    }
+}
